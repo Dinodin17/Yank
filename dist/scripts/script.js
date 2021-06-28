bdd9822af3477aa42b75911ddeb79583cb3394ed
Diana <diana17.dg17@gmail.com>
2021-06-28 03:34:44 +0300

    auctions

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,43 +1,5 @@
-const animItems = document.querySelectorAll('._anim-items');
-if(animItems.length > 0){
-  window.addEventListener('scroll', animOnScroll);
-    function animOnScroll(){
-      for(let index = 0; index < animItems.length; index++){
-        const animItem = animItems[index];
-        const animItemHeight = animItem.offsetHeight;
-        const animItemOffset = offset(animItem).top;
-        const animStart = 30;
-        let animItemPoint = window.innerHeight - animItemHeight / animStart;
-        if(animItemHeight > window.innerHeight){
-          animItemPoint = window.innerHeight - window.innerHeight / animStart;
-        }
-        if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
-          animItem.classList.add('animate__animated');
-        } else{
-          if(!animItem.classList.contains('_anim-nohide')){
-            animItem.classList.remove('animate__animated');
-          }
-        }
-      }
-    }
-    function offset(el){
-      const rect = el.getBoundingClientRect(),
-      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
-    }
-    
-}
+
 // анимация заголовков
-const animTitles = document.querySelectorAll('.title');
-if(animTitles.length > 0){
-for(let index = 0; index < animTitles.length; index++){
-    const animTitle = animTitles[index];
-    if (animTitle.classList.contains("_active")){
-    animTitle.classList.add('animate__animated');
-}
-}
-}
 
 
 
