c9d1e5ec0c9b93009406462d4bdeea06f70d2146
Diana <diana17.dg17@gmail.com>
2021-07-11 04:38:38 +0300

    yank

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -3,47 +3,47 @@
 
 
 
-const slider = $(".slider");
+// const slider = $(".slider");
 
-slider.on('wheel', (function(e) {
-  e.preventDefault();
+// slider.on('wheel', (function(e) {
+//   e.preventDefault();
 
-  if (e.originalEvent.deltaY < 0) {
-    $(this).slick('slickNext');
-  } else {
-    $(this).slick('slickPrev');
-  }
-}));
+//   if (e.originalEvent.deltaY < 0) {
+//     $(this).slick('slickNext');
+//   } else {
+//     $(this).slick('slickPrev');
+//   }
+// }));
 
 
-$(function (){
-	$('.slider').slick({
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: true,
-		centerMode: true,
-		centerPadding: '1px',
-		ariableWidth: true,
-        fade: true,
-        cssEase: 'linear',
-        speed: 300,
-        autoplay: true,
-        autoplaySpeed: 20000,
-		// responsive: [
-		// 	{
-		// 	breakpoint: 768,
-		// 	settings: {
-		// 	centerMode: false,
-		// 	arrows: false,
-		// 	slidesToShow: 3,
-		// 	vertical: true,
-		// 	verticalSwiping: true
-		// 	}
-		// 	},
-		// ]
-	});
-});
+// $(function (){
+// 	$('.slider').slick({
+// 		infinite: true,
+// 		slidesToShow: 1,
+// 		slidesToScroll: 1,
+// 		arrows: true,
+// 		centerMode: true,
+// 		centerPadding: '1px',
+// 		ariableWidth: true,
+//         fade: true,
+//         cssEase: 'linear',
+//         speed: 300,
+//         autoplay: true,
+//         autoplaySpeed: 20000,
+// 		// responsive: [
+// 		// 	{
+// 		// 	breakpoint: 768,
+// 		// 	settings: {
+// 		// 	centerMode: false,
+// 		// 	arrows: false,
+// 		// 	slidesToShow: 3,
+// 		// 	vertical: true,
+// 		// 	verticalSwiping: true
+// 		// 	}
+// 		// 	},
+// 		// ]
+// 	});
+// });
 
 var cbpAnimatedHeader = (function() {
 
@@ -81,32 +81,71 @@ var cbpAnimatedHeader = (function() {
 
 })();
 
-$(function (){
-	$('.stages__slider').slick({
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		centerMode: true,
-		centerPadding: '1px',
-        fade: true,
-        cssEase: 'linear',
-        speed: 900,
-        autoplay: true,
-        autoplaySpeed: 20000,
-        dots: true,
-        dotsClass: 'slick-dots',
-		// responsive: [
-		// 	{
-		// 	breakpoint: 768,
-		// 	settings: {
-		// 	centerMode: false,
-		// 	arrows: false,
-		// 	slidesToShow: 3,
-		// 	vertical: true,
-		// 	verticalSwiping: true
-		// 	}
-		// 	},
-		// ]
-	});
-});
+// $(function (){
+// 	$('.stages__slider').slick({
+// 		infinite: true,
+// 		slidesToShow: 1,
+// 		slidesToScroll: 1,
+// 		arrows: false,
+// 		centerMode: true,
+// 		centerPadding: '1px',
+//         fade: true,
+//         cssEase: 'linear',
+//         speed: 900,
+//         autoplay: true,
+//         autoplaySpeed: 20000,
+//         dots: true,
+//         dotsClass: 'slick-dots',
+// 		// responsive: [
+// 		// 	{
+// 		// 	breakpoint: 768,
+// 		// 	settings: {
+// 		// 	centerMode: false,
+// 		// 	arrows: false,
+// 		// 	slidesToShow: 3,
+// 		// 	vertical: true,
+// 		// 	verticalSwiping: true
+// 		// 	}
+// 		// 	},
+// 		// ]
+// 	});
+// });
+
+
+
+const swiper = new Swiper('.stages-slider',    {
+    // Optional parameters
+    direction: 'vertical',
+    autoplay: {
+        delay: 20000,
+        stopOnLastSlide: false,
+      },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  parallax: true,
+speed:2000,
+    
+  });
+
+  const banner = new Swiper('.slider',    {
+    // Optional parameters
+
+    // If we need pagination
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      autoplay: {
+        delay: 20000,
+        stopOnLastSlide: false,
+      },
+      effect: 'fade',
+      fadeEffect :{
+          crossFade: true,
+         
+      }
+  });
