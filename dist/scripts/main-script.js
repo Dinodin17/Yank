430701d933ab2e404835bcd19493bda82db3c577
Diana <diana17.dg17@gmail.com>
2021-09-17 19:25:11 +0300

    update

--- dist/scripts/main-script.js
+++ dist/scripts/main-script.js
@@ -234,7 +234,7 @@ if ($(window).width() > '799'){
         },
         
         autoplay: {
-            delay: 4000,
+            delay: 20000,
             stopOnLastSlide: false,
           },
        
@@ -244,7 +244,7 @@ if ($(window).width() > '799'){
         },
         parallax: true,
         speed:2000,
-          
+       
       });
 
       $('.swiper-pagination-bullet').hover(function() {
@@ -252,7 +252,51 @@ if ($(window).width() > '799'){
      });
   
 
-   
+     swiper.on('slideChange', function () {
+        var car_dot = document.getElementById('dot');
+        var dot = swiper.realIndex;
+        console.log(dot);
+            if(dot == 0){
+                car_dot.classList.toggle('zero');
+            } else {
+                car_dot.classList.remove('zero');
+            }
+            if(dot == 1){
+                car_dot.classList.toggle('first');
+            } else {
+                car_dot.classList.remove('first');
+            }
+    
+            if(dot == 2){
+                car_dot.classList.toggle('second');
+            } else {
+                car_dot.classList.remove('second');
+            }
+    
+            if(dot == 3){
+                car_dot.classList.toggle('third');
+            } else {
+                car_dot.classList.remove('third');
+            }
+    
+    
+            if(dot == 4){
+                car_dot.classList.toggle('fourth');
+            } else {
+                car_dot.classList.remove('fourth');
+            }
+    
+            if(dot == 5){
+                car_dot.classList.toggle('fifth');
+            } else {
+                car_dot.classList.remove('fifth');
+            }
+            if(dot == 6){
+                car_dot.classList.toggle('sixth');
+            } else {
+                car_dot.classList.remove('sixth');
+            }
+      });
    
 //    [].forEach.call(social ,function(){
 //        social.addEventListener('click', function (socialBack) {
@@ -368,62 +412,186 @@ if ($(window).width() <= '800'){
 
   }
  
-  swiper.on('slideChange', function () {
-    var car_dot = document.getElementById('dot');
-    var dot = swiper.activeIndex;
-        if(dot == 0){
-            car_dot.classList.toggle('zero');
-        } else {
-            car_dot.classList.remove('zero');
-        }
-        if(dot == 1){
-            car_dot.classList.toggle('first');
-        } else {
-            car_dot.classList.remove('first');
-        }
+  
+//  Попапы
 
-        if(dot == 2){
-            car_dot.classList.toggle('second');
-        } else {
-            car_dot.classList.remove('second');
-        }
+// Открытие попапа "Подобрать авто"
+function getAuto(){
+    var popup = document.getElementById("get-auto-popup");
+    var block = document.getElementById("block");
+    var close = document.getElementById("close");
 
-        if(dot == 3){
-            car_dot.classList.toggle('third');
-        } else {
-            car_dot.classList.remove('third');
-        }
+    popup.classList.toggle("show");
+    console.log("piy");
+    if(popup.classList.contains("show")){
+        block.classList.toggle("block");
+    }
 
+}
+// ЗАКТЫРИЕ ПОП АПА ПОДОБРАТЬ АВТО
+function getHide(){
+    var popup = document.getElementById("get-auto-popup");
+    var block = document.getElementById("block");
+    popup.classList.remove("show");
 
-        if(dot == 4){
-            car_dot.classList.toggle('fourth');
-        } else {
-            car_dot.classList.remove('fourth');
-        }
+    block.classList.remove("block");
+  
+}
+// ЗАКРЫТИЕ ПОДБОРА АВТО НА КЛИК ВНЕ ФОРМЫ
+var popup = document.getElementById("get-auto-popup");
+popup.addEventListener('click', function(event) {
+    var target = event.target;
 
-        if(dot == 5){
-            car_dot.classList.toggle('fifth');
-        } else {
-            car_dot.classList.remove('fifth');
-        }
-        if(dot == 6){
-            car_dot.classList.toggle('sixth');
-        } else {
-            car_dot.classList.remove('sixth');
-        }
-  });
- 
-    //   setInterval(function () {
+    if(target.className === "get-auto show"){
+        getHide();
+        console.log('piedsy');
+    }
+});
+
+//ОТКРЫТИЕ ПОП АПА КОНСУЛЬТАЦИИ
+function myConsult() {
+    console.log('piy');
+    var popup = document.getElementById("popupConsult");
+    var block = document.getElementById("block");
+    var close = document.getElementById("close");
+    popup.classList.toggle("show");
+
+    if(popup.classList.contains("show")){
+        block.classList.toggle("block");
+    }
+    close.addEventListener("onclick", function(){
+        popup.classList.toggle("show");
+    })
+    
+}  
+
+//ЗАКРЫТИЕ ПОП АПА КОНСУЛЬТАЦИИ
+function Hide(){
+    var popup = document.getElementById("popupConsult");
+    var block = document.getElementById("block");
+    popup.classList.remove("show");
+    block.classList.remove("block");
+   
+}
+
+// ЗАКРЫТИЕ НА КЛИК ВНЕ ФОРМЫ (КОНСУЛЬТАЦИЯ)
+var popupClose = document.getElementById("popupConsult");
+popupClose.addEventListener('click', function(event) {
+    var target = event.target;
+    if(target.className === "pop-up-concult show"){
+        Hide();
+    }
+});
+
+//   Выпадашка(селект)
+window.onload = function(){
+    crear_select();
+}
+    function isMobileDevice() {
+        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+    };
+    
+    
+    var li = new Array();
+    function crear_select(){
+    var div_cont_select = document.querySelectorAll("[data-mate-select='active']");
+    var select_ = '';
+    for (var e = 0; e < div_cont_select.length; e++) {
+    div_cont_select[e].setAttribute('data-indx-select',e);
+    div_cont_select[e].setAttribute('data-selec-open','false');
+    var ul_cont = document.querySelectorAll("[data-indx-select='"+e+"'] > .cont_list_select_mate > ul");
+    select_ = document.querySelectorAll("[data-indx-select='"+e+"'] >select")[0];
+    if (isMobileDevice()) { 
+    select_.addEventListener('change', function () {
+    _select_option(select_.selectedIndex,e);
+    });
+    }
+    var select_optiones = select_.options;
+    document.querySelectorAll("[data-indx-select='"+e+"']  > .selecionado_opcion ")[0].setAttribute('data-n-select',e);
+    document.querySelectorAll("[data-indx-select='"+e+"']  > .icon_select_mate ")[0].setAttribute('data-n-select',e);
+    for (var i = 0; i < select_optiones.length; i++) {
+    li[i] = document.createElement('li');
+    if (select_optiones[i].selected == true || select_.value == select_optiones[i].innerHTML ) {
+    li[i].className = 'active';
+    document.querySelector("[data-indx-select='"+e+"']  > .selecionado_opcion ").innerHTML = select_optiones[i].innerHTML;
+    };
+    li[i].setAttribute('data-index',i);
+    li[i].setAttribute('data-selec-index',e);
+    // funcion click al selecionar 
+    li[i].addEventListener( 'click', function(){  _select_option(this.getAttribute('data-index'),this.getAttribute('data-selec-index')); });
+    
+    li[i].innerHTML = select_optiones[i].innerHTML;
+    ul_cont[0].appendChild(li[i]);
+    
+        }; 
+        };
+    } 
+    
+    var cont_slc = 0;
+    function open_select(idx){
+    var idx1 =  idx.getAttribute('data-n-select');
+        var ul_cont_li = document.querySelectorAll("[data-indx-select='"+idx1+"'] .cont_select_int > li");
+    var hg = 0;
+    var slect_open = document.querySelectorAll("[data-indx-select='"+idx1+"']")[0].getAttribute('data-selec-open');
+    var slect_element_open = document.querySelectorAll("[data-indx-select='"+idx1+"'] select")[0];
+    if (isMobileDevice()) { 
+        if (window.document.createEvent) { // All
+        var evt = window.document.createEvent("MouseEvents");
+        evt.initMouseEvent("mousedown", false, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        slect_element_open.dispatchEvent(evt);
+    } else if (slect_element_open.fireEvent) { // IE
+        slect_element_open.fireEvent("onmousedown");
+    }else {
+        slect_element_open.click();
+    }
+    }else {
+    
         
-        // }, 4000)
-
-       
-        // if(dot == 3){
-        //     car_dot.classList.toggle('first');
-        // } else {
-        //     car_dot.classList.remove('first');
-        // }
-//   }
+        for (var i = 0; i < ul_cont_li.length; i++) {
+    hg += ul_cont_li[i].offsetHeight;
+    }; 
+    if (slect_open == 'false') {  
+    document.querySelectorAll("[data-indx-select='"+idx1+"']")[0].setAttribute('data-selec-open','true');
+    document.querySelectorAll("[data-indx-select='"+idx1+"'] > .cont_list_select_mate > ul")[0].style.height = hg+"px";
+    document.querySelectorAll("[data-indx-select='"+idx1+"'] > .icon_select_mate")[0].style.transform = 'rotate(180deg)';
+    }else{
+    document.querySelectorAll("[data-indx-select='"+idx1+"']")[0].setAttribute('data-selec-open','false');
+    document.querySelectorAll("[data-indx-select='"+idx1+"'] > .icon_select_mate")[0].style.transform = 'rotate(0deg)';
+    document.querySelectorAll("[data-indx-select='"+idx1+"'] > .cont_list_select_mate > ul")[0].style.height = "0px";
+    }
+    }
+    
+    } // fin function open_select
+    
+    function salir_select(indx){
+    var select_ = document.querySelectorAll("[data-indx-select='"+indx+"'] > select")[0];
+    document.querySelectorAll("[data-indx-select='"+indx+"'] > .cont_list_select_mate > ul")[0].style.height = "0px";
+    document.querySelector("[data-indx-select='"+indx+"'] > .icon_select_mate").style.transform = 'rotate(0deg)';
+    document.querySelectorAll("[data-indx-select='"+indx+"']")[0].setAttribute('data-selec-open','false');
+    }
+    
+    
+    function _select_option(indx,selc){
+        if (isMobileDevice()) { 
+        selc = selc -1;
+        }
+            var select_ = document.querySelectorAll("[data-indx-select='"+selc+"'] > select")[0];
+        
+            var li_s = document.querySelectorAll("[data-indx-select='"+selc+"'] .cont_select_int > li");
+            var p_act = document.querySelectorAll("[data-indx-select='"+selc+"'] > .selecionado_opcion")[0].innerHTML = li_s[indx].innerHTML;
+        var select_optiones = document.querySelectorAll("[data-indx-select='"+selc+"'] > select > option");
+        for (var i = 0; i < li_s.length; i++) {
+        if (li_s[i].className == 'active') {
+        li_s[i].className = '';
+        };
+        li_s[indx].className = 'active';
+        
+        };
+        select_optiones[indx].selected = true;
+            select_.selectedIndex = indx;
+            select_.onchange();
+            salir_select(selc); 
+    }
 
   
 //   var dot = document.getElementById('dot');
@@ -450,3 +618,5 @@ if ($(window).width() <= '800'){
 // social.onmouseover = function () {
 //   socialBack.classList.add('socialBack');
 // }
+
+var chosenCar = document.getElementById('chosenCar');
