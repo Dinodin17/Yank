2e3443fa718fa616d0bb2818a32206c08745be69
Diana <diana17.dg17@gmail.com>
2021-09-23 00:19:24 +0300

    update

--- dist/scripts/calculator.js
+++ dist/scripts/calculator.js
@@ -168,38 +168,6 @@ window.onload = function(){
             salir_select(selc); 
     }
 
-    // Открытие попапа "Подобрать авто"
-function getAuto(){
-    var popup = document.getElementById("get-auto-popup");
-    var block = document.getElementById("block");
-    var close = document.getElementById("close");
-
-    popup.classList.toggle("show");
-    console.log("piy");
-    if(popup.classList.contains("show")){
-        block.classList.toggle("block");
-    }
-
-}
-// ЗАКТЫРИЕ ПОП АПА ПОДОБРАТЬ АВТО
-function getHide(){
-    var popup = document.getElementById("get-auto-popup");
-    var block = document.getElementById("block");
-    popup.classList.remove("show");
-
-    block.classList.remove("block");
-  
-}
-// ЗАКРЫТИЕ ПОДБОРА АВТО НА КЛИК ВНЕ ФОРМЫ
-var popup = document.getElementById("get-auto-popup");
-popup.addEventListener('click', function(event) {
-    var target = event.target;
-
-    if(target.className === "get-auto show"){
-        getHide();
-        console.log('piedsy');
-    }
-});
 
 //ОТКРЫТИЕ ПОП АПА КОНСУЛЬТАЦИИ
 function myConsult() {
@@ -234,4 +202,59 @@ popupClose.addEventListener('click', function(event) {
     if(target.className === "pop-up-concult show"){
         Hide();
     }
+});
+
+$('.select').each(function() {
+    const _this = $(this),
+        selectOption = _this.find('option'),
+        selectOptionLength = selectOption.length,
+        selectedOption = selectOption.filter(':selected'),
+        duration = 450; // длительность анимации 
+
+    _this.hide();
+    _this.wrap('<div class="select"></div>');
+    $('<div>', {
+        class: 'new-select',
+        text: _this.children('option:disabled').text()
+    }).insertAfter(_this);
+
+    const selectHead = _this.next('.new-select');
+    $('<div>', {
+        class: 'new-select__list'
+    }).insertAfter(selectHead);
+
+    const selectList = selectHead.next('.new-select__list');
+    for (let i = 1; i < selectOptionLength; i++) {
+        $('<div>', {
+            class: 'new-select__item',
+            html: $('<span>', {
+                text: selectOption.eq(i).text()
+            })
+        })
+        .attr('data-value', selectOption.eq(i).val())
+        .appendTo(selectList);
+    }
+
+    const selectItem = selectList.find('.new-select__item');
+    selectList.slideUp(0);
+    selectHead.on('click', function() {
+        if ( !$(this).hasClass('on') ) {
+            $(this).addClass('on');
+            selectList.slideDown(duration);
+
+            selectItem.on('click', function() {
+                let chooseItem = $(this).data('value');
+
+                $('select').val(chooseItem).attr('selected', 'selected');
+                selectHead.text( $(this).find('span').text() );
+
+                selectList.slideUp(duration);
+                selectHead.removeClass('on');
+            });
+
+        } else {
+            $(this).removeClass('on');
+            selectList.slideUp(duration);
+        }
+    });
 });
